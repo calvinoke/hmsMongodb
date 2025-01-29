e08052811531a305451c0ff7f689c63e78d1f12e
Calvin Oker <calvinoker8@gmail.com>
2025-01-29 14:15:34 +0300

    updating the app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require("express");
-const connectDb = require("./config/dbConnection");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const path = require("path");
+import express from 'express';
+//import mongoose from 'mongoose';
+import connectDb from './config/dbConnection.js';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+// Load environment variables
 dotenv.config();
+
 const app = express();
 
 const corsOptions = {
@@ -17,6 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 
+// Resolve __dirname equivalent in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Serve uploaded images
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
@@ -24,33 +32,27 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 connectDb();
 
 // Import and use routers
-const userRouter = require("./routes/user");
+import userRouter from "./routes/user.js";
+import patientRouter from "./routes/patient.js";
+import visitRouter from "./routes/visite.js";
+import appointmentRouter from "./routes/appointment.js";
+import prescriptionRouter from "./routes/prescription.js";
+import notificationRouter from "./routes/notification.js";
+import detectionRouter from "./routes/detection.js";
+import historyRouter from "./routes/history.js";
+
 app.use("/users", userRouter);
-
-const patientRouter = require("./routes/patient");
 app.use("/patients", patientRouter);
-
-const visitRouter = require("./routes/visite");
-app.use("/visits/", visitRouter);
-
-const appointmentRouter = require("./routes/appointment");
+app.use("/visits", visitRouter);
 app.use("/appointments", appointmentRouter);
-
-const prescriptionRouter = require("./routes/prescription");
-app.use("/prescriptions/", prescriptionRouter);
-
-const notificationRouter = require("./routes/notification");
-app.use("/notifications/", notificationRouter);
-
-const detectionRouter = require("./routes/detection");
-app.use("/detection/", detectionRouter);
-
-const historyRouter = require("./routes/history");
-app.use("/history/", historyRouter);
+app.use("/prescriptions", prescriptionRouter);
+app.use("/notifications", notificationRouter);
+app.use("/detection", detectionRouter);
+app.use("/history", historyRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-module.exports = app;
+export default app;
